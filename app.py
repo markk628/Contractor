dc60843827e97d4ea8c13b6c497ffe4d987adcf3
Donghyun <markk628@gmail.com>
2019-10-07 23:36:49 -0700

    added cart page

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def contractor_index():
     return render_template('contractor_index.html', )
+
+@app.route('/cart')
+def contractor_new():
+    return render_template('contractor_new.html', title='Your Cart')
